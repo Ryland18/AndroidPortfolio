422b1fb4d4e4e49e1bfb9acbeafe307a53ef24d5
Ryland18 <ryrex1808@icloud.com>
2025-11-20 18:48:51 -0600

    scifi and home

--- PortfolioUpgraded/app/src/main/java/com/example/portfolioupgraded/ui/slideshow/scifinameFragment.java
+++ PortfolioUpgraded/app/src/main/java/com/example/portfolioupgraded/ui/slideshow/scifinameFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -27,9 +28,44 @@ public class scifinameFragment extends Fragment {
         final TextView textView = binding.textSlideshow;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
+        EditText first = binding.editTextText2;
+        EditText last = binding.editTextText3;
+        EditText city = binding.editTextText4;
+        EditText school = binding.editTextText5;
+        EditText pet = binding.editTextText6;
+        EditText chara = binding.editTextText7;
+
+
+
+
+
         binding.button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                String inputfirst = first.getText().toString();
+                String inputlast = last.getText().toString();
+                String inputcity = city.getText().toString();
+                String inputschool = school.getText().toString();
+                String inputpet = pet.getText().toString();
+                String inputchara = chara.getText().toString();
+
+                String firsttwo = inputfirst.length()>=2 ? inputfirst.substring(0,2): inputfirst;
+                String lastthree = inputfirst.length()>=3 ? inputfirst.substring(0,3): inputfirst;
+                String citytwo = inputfirst.length()>=2 ? inputfirst.substring(0,2): inputfirst;
+                String schoolthree = inputfirst.length()>=3 ? inputfirst.substring(0,3): inputfirst;
+
+
+                String firstName = firsttwo+lastthree;
+                String lastName = citytwo + schoolthree;
+                String origin = inputpet + " "+ inputchara;
+
+
+                textView.setText( firstName + " "+ lastName + " from the planet "+ origin);
+
+
+
 
             }
         });
